7cc2b839c5e3b44a4cdf25eb2f60af73e5b87d3c
Tunji <macchupiccchu2003@gmail.com>
2026-06-09 07:23:23 +0100

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    # This completely forces Django to ignore the user field when creating a task
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Task
         fields = ['id', 'title', 'completed', 'user']
-        # CRITICAL: This MUST be indented inside "class Meta"
-        read_only_fields = ['user']
