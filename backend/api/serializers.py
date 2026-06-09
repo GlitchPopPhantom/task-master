7d74886b5c4857a6425a5d427c6756a605a7bb74
Tunji <macchupiccchu2003@gmail.com>
2026-06-09 07:39:05 +0100

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,8 +2,9 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    # This explicitly overrides the model and forces it to be read-only
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Task
         fields = ['id', 'title', 'completed', 'user']
-        # This completely stops the frontend from being forced to send a user ID
-        read_only_fields = ['user']
