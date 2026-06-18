0d3fe47c684c44230566d355889b690d79d6896d
Tunji <macchupiccchu2003@gmail.com>
2026-06-18 13:28:37 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -7,3 +7,6 @@ pip install -r requirements.txt
 
 # 2. Compile static assets safely with inline dummy URL
 DATABASE_URL="postgres://dummy:dummy@localhost:5432/dummy" python manage.py collectstatic --no-input
+
+# 3. FORCE TABLE CREATION WITHOUT MIGRATION FILES
+python manage.py migrate --run-syncdb
